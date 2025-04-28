b03a5c2ba5e2a409796c650c292df26c9a89f73f
Paweł Mwaba <pmwaba@grupazpr.pl>
2025-04-29 01:54:03 +0200

    chore: add @astrojs/node adapter for standalone server mode and update package dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import node from "@astrojs/node";
 
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
@@ -8,6 +9,9 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   output: "server",
+  adapter: node({
+    mode: "standalone",
+  }),
   integrations: [react(), sitemap()],
   server: { port: 3000 },
   vite: {
